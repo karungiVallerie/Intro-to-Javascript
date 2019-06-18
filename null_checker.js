775559122d0eeaaab007f10506a80e2ed8d7e2f4
karungivallerie <karungivallerie@gmail.com>
2019-06-18 15:20:45 +0300

    add work

---
+++ null_checker.js
@@ -0,0 +1,32 @@
+ var i;
+  for(i=1; i<=10;i++)
+
+
+     {
+  
+    let k = prompt('Enter your number ?',"");
+        if  (k>0)
+    
+    {
+    console.log(1);
+    }
+
+
+    else if(k<0)
+     {
+         console.log(-1);
+     }
+
+
+    else 
+    {
+        console.log(0);
+    }
+
+
+}
+        
+         
+
+
+
