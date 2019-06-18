6bb63d01d1da368eeadbf64a0dc7e1dbeee72a3c
karungivallerie <karungivallerie@gmail.com>
2019-06-18 11:30:52 +0300

    add javascript work

---
+++ billing.js
@@ -0,0 +1,13 @@
+//This is the percentage that determines the transaction charges
+const transactionCharge =0.025;
+
+
+    var amountTendered =100000;
+
+
+    
+
+        console.log ('This is what the billed amount is(less  transaction  charges)');
+        
+console.log(amountTendered-(amountTendered*transactionCharge));
+ console.log(100000-(0.025*100000))
